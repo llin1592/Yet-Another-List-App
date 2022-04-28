4d82786a12c30c115576d948bb112492ad27571d
llin1592 <ll9wv@virginia.edu>
2022-04-27 22:57:04 -0400

    refactored App.js to function instead of class

--- src/App.js
+++ src/App.js
@@ -1,87 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Header from "./Components/Header.js";
 import Dashboard from "./Components/Dashboard.js";
 import Menu from "./Components/Menu.js";
 import "./Styles/App.css";
 
 /* https://stackoverflow.com/questions/66701117/re-render-child-after-parent-state-change-with-get-request */
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchText: "",
-      groups: [],
-      editNoteLocation: {},
-    };
-    this.onSearch = this.onSearch.bind(this);
-  }
+function App() {
+  const [searchText, setSearchText] = useState("");
+  const [groups, setGroups] = useState([]);
+  const [editNote, setEditNote] = useState({});
+  const [config, setConfig] = useState({ xscroll: false, confirmDelete: true });
 
-  onSearch(search) {
-    this.setState({
-      searchText: search,
-      group: this.state.group,
-      editNoteLocation: this.state.editNoteLocation,
-    });
-  }
+  return (
+    <div className="app-container">
+      <Header
+        searchText={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        resetSearch={() => setSearchText("")}
+        config={config}
+      />
+      <Dashboard
+        groups={groups}
+        config={config}
+        setGroups={(newGroups) => setGroups(newGroups)}
+        searchText={searchText.toLowerCase()}
+        openEditMenu={(noteLocation) => setEditNote(noteLocation)}
+      />
 
-  setGroups(groups) {
-    this.setState({
-      searchText: this.state.searchText,
-      groups: groups,
-      editNoteLocation: this.state.editNoteLocation,
-    });
-  }
-
-  setEditMenu(noteLocation) {
-    this.setState({
-      searchText: this.state.searchText,
-      groups: this.state.groups,
-      editNoteLocation: noteLocation,
-    });
-  }
-
-  render() {
-    return (
-      <div className="app-container">
-        <Header
-          searchText={this.state.searchText}
-          onChange={(e) => this.onSearch(e.target.value)}
-          resetSearch={() => this.onSearch("")}
+      {Object.keys(editNote).length !== 0 ? (
+        <Menu
+          groups={groups}
+          setGroups={(newGroups) => setGroups(newGroups)}
+          groupIndex={editNote.groupIndex}
+          noteIndex={editNote.noteIndex}
+          closeEditMenu={() => setEditNote({})}
         />
-        <Dashboard
-          groups={this.state.groups}
-          setGroups={(newGroups) => this.setGroups(newGroups)}
-          searchText={this.state.searchText.toLowerCase()}
-          openEditMenu={(noteLocation) => this.setEditMenu(noteLocation)}
-        />
-
-        {Object.keys(this.state.editNoteLocation).length !== 0 ? (
-          <Menu
-            groups={this.state.groups}
-            setGroups={(newGroups) => this.setGroups(newGroups)}
-            groupIndex={this.state.editNoteLocation.groupIndex}
-            noteIndex={this.state.editNoteLocation.noteIndex}
-            closeEditMenu={() => this.setEditMenu({})}
-          />
-        ) : null}
-      </div>
-    );
-  }
+      ) : null}
+    </div>
+  );
 }
-
-// function App() {
-//   const [searchText, setSearchText] = useState("");
-
-//   return (
-//     <div className="app-container">
-//       <Header
-//         searchText={searchText}
-//         onChange={(e) => setSearchText(e.target.value)}
-//       />
-//       <p>{searchText}</p>
-//       <Dashboard search={searchText} />
-//     </div>
-//   );
-// }
 
 export default App;
